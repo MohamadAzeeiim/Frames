f4ef7190c652fcef53a9e719d58afc75c5627635
ashaikh <arshaan_shaikh@zimbra.sevenpv.com>
2025-03-21 08:39:58 +0530

    card section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css"; // Keep this for global styles (optional)
 import Navbar from "./components/Navbar"; // Import Navbar component
 import HeroSection from "./components/HeroSection";
+import CardSection from "./components/CardSection/CardSection";
 
 const App = () => {
   return (
     <>
       <Navbar /> 
       <HeroSection />
+      <CardSection/>
     </>
   );
 };
